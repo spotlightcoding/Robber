b1e1fcc23ee66705edca602d9bf41129a6448984
Nathan McDonald <nmcdonald00@gmail.com>
2012-12-15 03:38:30 -0400

    added main class

---
+++ src/com/spotlightcoding/Rob.java
@@ -0,0 +1,28 @@
+package com.spotlightcoding;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Rob extends StateBasedGame{
+
+	public Rob(String name) {
+		super(name);
+		
+	}
+	
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		
+		
+	}
+	
+	public static void main(String[] args){
+		AppGameContainer appgc;
+		
+	}
+
+	
+
+}
