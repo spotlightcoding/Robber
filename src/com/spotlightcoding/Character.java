766c01934deaab0d928dfc4ebd5e88fb08fabc4c
Nathan McDonald <nmcdonald00@gmail.com>
2012-12-15 06:37:19 -0400

    added world class and packages for entities and components

---
+++ src/com/spotlightcoding/Character.java
@@ -0,0 +1,15 @@
+package com.spotlightcoding;
+
+public class Character extends Entity{
+	//------------------------------------------------CONSTANTS
+	//------------------------------------------------PROPERTIES
+	
+	//------------------------------------------------CONSTRUCTOR
+	public Character(String id) {
+		super(id);
+	}
+	//------------------------------------------------GETS/SETS
+	//------------------------------------------------PUBLIC METHODS
+	//------------------------------------------------PRIVATE METHODS
+
+}
